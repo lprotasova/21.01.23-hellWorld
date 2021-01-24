bdf0a98c468937381c47136563698f34221beff9
lprotasova <lprotasova@carthage.edu>
2021-01-24 14:01:08 -0600

    added collision detection
    physicsItems now have a size, measured in radius from the center. If the centers of two items are closer together than the sum of the radii, on both axes, they are colliding. in the test for this commit, I simulate an asteroid moving toward a ship until it collides

--- Driver.java
+++ Driver.java
@@ -1,11 +1,37 @@
 public class Driver {
     public static void main(String[] args) {
-        PhysicsItem item1 = new PhysicsItem(4.1, 5.2);
+        //test constructor 1, modifyVelocity, and both moves
+        /* PhysicsItem item1 = new PhysicsItem(4.1, 5.2);
         item1.testPrint();
         item1.modifyVelocity(new Pair(-3, -6));
         item1.move();
         item1.testPrint();
         item1.move(5);
-        item1.testPrint();
+        item1.testPrint(); */
+
+        //test constructor 2 and undoing velocity with multiple modify calls
+        /* PhysicsItem item2 = new PhysicsItem(-5, 4, 2, 2);
+        item2.testPrint();
+        item2.modifyVelocity(new Pair(2, 1));
+        item2.testPrint();
+        item2.modifyVelocity(new Pair(-2, -1));
+        item2.testPrint(); */
+
+        //test constructor 3 and collision
+        PhysicsItem item3Ship = new PhysicsItem(5.5, 15, .5, .5, 0, 0);
+        PhysicsItem item4Asteroid = new PhysicsItem(5, 2.5, .5, .5, 0, .5);
+
+        for (int i = 0; i < 24; i++) {
+            System.out.println("-----Ship Status:");
+            item3Ship.testPrint();
+            System.out.println("-----Asteroid Status:");
+            item4Asteroid.testPrint();
+
+            System.out.println("-----Collision Status: " + item3Ship.collidesWith(item4Asteroid));
+
+            System.out.println("-----Moving...");
+            item3Ship.move();
+            item4Asteroid.move();
+        }
     }
 }
--- PhysicsItem.java
+++ PhysicsItem.java
@@ -1,16 +1,39 @@
 public class PhysicsItem {
     private Pair location;
     private Pair velocity;
+    private Pair radius;
 
-    //start with 0 velocity
+    //start with 0 velocity and 0 radius
     public PhysicsItem(double startXLoc, double startYLoc) {
         location = new Pair(startXLoc, startYLoc);
 
         velocity = new Pair();
+
+        radius = new Pair();
+    }
+
+    //start with 0 velocity
+    public PhysicsItem(double startXLoc, double startYLoc, double xRadius, 
+        double yRadius) {
+        location = new Pair(startXLoc, startYLoc);
+        radius = new Pair(xRadius, yRadius);
+        velocity = new Pair();
+    }
+
+    //start with location, velocity, and radius set
+    public PhysicsItem(double startXLoc, double startYLoc, double xRadius, 
+        double yRadius, double startXVel, double startYVel) {
+        location = new Pair(startXLoc, startYLoc);
+        velocity = new Pair(startXVel, startYVel);
+        radius = new Pair(xRadius, yRadius);
     }
 
     public Pair getCurrentLocation() {
         return location;
+    }
+
+    public Pair getRadius() {
+        return radius;
     }
 
     //assume in one tick (one call to move), location changes by velocity units
@@ -30,8 +53,41 @@ public class PhysicsItem {
         velocity.y += velocityDelta.y;
     }
 
+    public void setRadius(Pair newRadius) {
+        radius = newRadius;
+    }
+
+    /** this function finds collisions. an item collides with this item
+     * if the centers of the items (denoted by the location pair) are
+     * within the x radii away from each other, and the same on the y
+     * axis
+     */
+    public boolean collidesWith(PhysicsItem otherItem) {
+        boolean isCollidingX = false;
+        boolean isCollidingY = false;
+
+        //first the x calculation
+        double distanceBetweenCentersX = Math.abs(location.x - 
+            otherItem.getCurrentLocation().x);
+        double sumOfXRadii = Math.abs(radius.x) + 
+            Math.abs(otherItem.getRadius().x);
+        isCollidingX = distanceBetweenCentersX <= sumOfXRadii;
+
+        //only calculate the y axis if the x axis is already colliding
+        if (isCollidingX) {
+            double distanceBetweenCentersY = Math.abs(location.y - 
+                otherItem.getCurrentLocation().y);
+            double sumOfYRadii = Math.abs(radius.y) + 
+                Math.abs(otherItem.getRadius().y);
+            isCollidingY = distanceBetweenCentersY <= sumOfYRadii;
+        }
+
+        return isCollidingX && isCollidingY;
+    }
+
     public void testPrint() {
         System.out.println("Current location: [" + location.x + ", " + location.y + "]");
         System.out.println("Going: [" + velocity.x + ", " + velocity.y + "]");
+        System.out.println("Radius (1/2 of size): [" + radius.x + ", " + radius.y + "]");
     }
 }
